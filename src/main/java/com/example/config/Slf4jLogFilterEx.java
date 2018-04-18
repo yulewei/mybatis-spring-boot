615537bf960a8ad33c979c083300c0d6b52f0695
yulewei <yulewei@gmail.com>
2018-04-18 11:34:54 +0800

    优化 druid 的 sql 日志输出

--- src/main/java/com/example/config/Slf4jLogFilterEx.java
+++ src/main/java/com/example/config/Slf4jLogFilterEx.java
@@ -4,6 +4,7 @@ import com.alibaba.druid.filter.logging.Slf4jLogFilter;
 import com.alibaba.druid.proxy.jdbc.CallableStatementProxy;
 import com.alibaba.druid.proxy.jdbc.JdbcParameter;
 import com.alibaba.druid.proxy.jdbc.PreparedStatementProxy;
+import com.alibaba.druid.proxy.jdbc.ResultSetProxy;
 import com.alibaba.druid.proxy.jdbc.StatementProxy;
 import com.alibaba.druid.sql.SQLUtils;
 
@@ -24,13 +25,51 @@ public class Slf4jLogFilterEx extends Slf4jLogFilter {
         this.setStatementCreateAfterLogEnabled(false);
         this.setStatementExecutableSqlLogEnable(true);
         this.setStatementExecuteAfterLogEnabled(true);
+        this.setStatementExecuteQueryAfterLogEnabled(true);
         this.setStatementParameterSetLogEnabled(false);
         this.setStatementPrepareAfterLogEnabled(false);
-        this.setStatementSqlFormatOption(new SQLUtils.FormatOption(false, false, false));
+//        this.setStatementSqlFormatOption(new SQLUtils.FormatOption(false, false, false));
+    }
+
+    @Override
+    protected void statementExecuteQueryAfter(StatementProxy statement, String sql, ResultSetProxy resultSet) {
+        if (isStatementExecuteQueryAfterLogEnabled() && isStatementLogEnabled()) {
+            statement.setLastExecuteTimeNano();
+            double nanos = statement.getLastExecuteTimeNano();
+            double millis = nanos / (1000 * 1000);
+
+            String formattedSql = this.buildFormattedSql(statement, sql);
+            String message = String.format("{conn-%s, %s} query executed. %.2f millis.\n\t%s",
+                    statement.getConnectionProxy().getId(), stmtId(statement), millis, formattedSql);
+            this.statementLog(message);
+        }
     }
 
     @Override
     protected void statementExecuteAfter(StatementProxy statement, String sql, boolean firstResult) {
+        if (isStatementExecuteAfterLogEnabled() && isStatementLogEnabled()) {
+            statement.setLastExecuteTimeNano();
+            double nanos = statement.getLastExecuteTimeNano();
+            double millis = nanos / (1000 * 1000);
+
+            String formattedSql = this.buildFormattedSql(statement, sql);
+            String message = String.format("{conn-%s, %s} executed. %.2f millis.\n\t%s",
+                    statement.getConnectionProxy().getId(), stmtId(statement), millis, formattedSql);
+            this.statementLog(message);
+        }
+    }
+
+    @Override
+    protected void statement_executeErrorAfter(StatementProxy statement, String sql, Throwable error) {
+        if (this.isStatementLogErrorEnabled()) {
+            String formattedSql = this.buildFormattedSql(statement, sql);
+            String message = String.format("{conn-%s, %s} execute error.\n\t%s",
+                    statement.getConnectionProxy().getId(), stmtId(statement), formattedSql);
+            this.statementLogError(message, error);
+        }
+    }
+
+    private String buildFormattedSql(StatementProxy statement, String sql) {
         int parametersSize = statement.getParametersSize();
         List<Object> parameters = new ArrayList<Object>(parametersSize);
         for (int i = 0; i < parametersSize; ++i) {
@@ -39,35 +78,8 @@ public class Slf4jLogFilterEx extends Slf4jLogFilter {
         }
 
         String dbType = statement.getConnectionProxy().getDirectDataSource().getDbType();
-        String formattedSql = SQLUtils.format(sql, dbType, parameters, this.getStatementSqlFormatOption());
-
-        if (isStatementExecuteAfterLogEnabled() && isStatementLogEnabled()) {
-            statement.setLastExecuteTimeNano();
-            double nanos = statement.getLastExecuteTimeNano();
-            double millis = nanos / (1000 * 1000);
-
-            String message = String.format("{conn-%s, %s} executed. %.2f millis.\n\t%s",
-                    statement.getConnectionProxy().getId(), stmtId(statement), millis, formattedSql);
-            statementLog(message);
-        }
-    }
-
-    @Override
-    protected void statement_executeErrorAfter(StatementProxy statement, String sql, Throwable error) {
-        if (this.isStatementLogErrorEnabled()) {
-            int parametersSize = statement.getParametersSize();
-            List<Object> parameters = new ArrayList<Object>(parametersSize);
-            for (int i = 0; i < parametersSize; ++i) {
-                JdbcParameter jdbcParam = statement.getParameter(i);
-                parameters.add(jdbcParam != null ? jdbcParam.getValue() : null);
-            }
-            String dbType = statement.getConnectionProxy().getDirectDataSource().getDbType();
-            String formattedSql = SQLUtils.format(sql, dbType, parameters, this.getStatementSqlFormatOption());
-
-            String message = String.format("{conn-%s, %s} execute error.\n\t%s",
-                    statement.getConnectionProxy().getId(), stmtId(statement), formattedSql);
-            statementLogError(message, error);
-        }
+        return SQLUtils.format(sql, dbType, parameters, this.getStatementSqlFormatOption());
+//        return SQLUtils.format(sql, dbType, parameters);
     }
 
     private String stmtId(StatementProxy statement) {
